1fff9e57c1aa6d728f46e67df1a974f6e088b3e2
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-05-22 14:36:11 -0500

    Now builds manifest files as config/GALLERY/ENV.json for all

--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,66 @@ var path = require('path');
 var clean = require('gulp-clean');
 var ManifestBuilder = require('./manifest_builder.js');
 var through = require('through2');
+var rename = require('gulp-rename');
 
 gulp.task('default', ['build']);
 
 gulp.task('build', ['environment', 'clean'], function(){
-  builder = new ManifestBuilder(config());
-  gulp.src(path.join(__dirname, gallery, 'manifest.json'))
-    .pipe(through.obj(function(data, encoding, done){
-      original = JSON.parse(data.contents.toString());
-      new_manifest = builder.transform(original);
-      data.contents = new Buffer(JSON.stringify(new_manifest));
-      this.push(data);
-      done();
-    }))
-   .pipe(gulp.dest(config_dir))
+  fs.readdir(path.join(__dirname, 'galleries'), function(error, files){
+    if (error) {
+      throw 'no galleries found?' + error.message
+    } else {
+      files.forEach(function(gallery){
+        build(gallery);
+      });
+    }
+  });
 });
 
 gulp.task('clean', function(){
-  gulp.src('config/*', { read: false })
+  return gulp.src('config/*', { read: false })
     .pipe(clean());
 });
 
-var env,
-  gallery,
-  config_dir;
+var config_dir;
 
 gulp.task('environment', function(){
-  env = process.env.GALLERY_ENV || 'development';
-  gallery = process.env.GALLERY;
-  config_dir = process.env.CONFIG_DIR || 'config';
-
-  if (!gallery) {
-    throw 'Must specify a GALLERY';
-  } else {
-    // naive avoidance of messing with changing directories
-    env = env.replace('.', '');
-    gallery = gallery.replace('.', '');
-    console.log('Building gallery ' + gallery);
-  }
+  return config_dir = process.env.CONFIG_DIR || 'config';
 });
 
-var config = function(){
-  config_path = path.join(__dirname, gallery, 'config', env + '.json');
-  console.log('Using config from ' + config_path);
-  try {
-    data = fs.readFileSync(config_path);
-    return JSON.parse(data);
-  } catch (err) {
-    console.log(err.message);
+var build = function(gallery){
+  console.log('Building ' + gallery);
+
+  config = require(path.join(galleryPath(gallery), 'config.json'));
+  var manifestPath = path.join(galleryPath(gallery), 'manifest.json');
+
+  withEachEnv(config, function(env, envConf) {
+    builder = new ManifestBuilder(envConf);
+    gulp.src(manifestPath)
+      .pipe(through.obj(function(data, encoding, done){
+        original = JSON.parse(data.contents.toString());
+        new_manifest = builder.transform(original);
+        data.contents = new Buffer(JSON.stringify(new_manifest));
+        this.push(data);
+        done();
+      }))
+      .pipe(rename(gallery + '/' + env + '.json'))
+      .pipe(gulp.dest(config_dir));
+   });
+}
+
+var withEachEnv = function(config, callback){
+  for (env in config) {
+    if (config.hasOwnProperty(env)){
+      callback(env, config[env]);
+    }
   }
-};
+}
+
+var galleryPath = function(gallery) {
+  return path.join(__dirname, 'galleries', gallery);
+}
+
+var galleryDestPath = function(gallery, env){
+  return path.join(__dirname, config_dir, gallery, env + '.json');
+}
